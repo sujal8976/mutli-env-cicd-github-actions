c11cd79aa1687352f4f19a0d4c72628a0eb5f63d
Sujal Gupta <sujalgupta8976@gmail.com>
2025-11-25 15:33:01 +0530

    fixed env badges issue

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app, { connectDB } from "./server";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.NODE_ENV === 'test' ? 5000 : (process.env.PORT || 3000);
 
 if (require.main === module) {
   connectDB().then(() => {
